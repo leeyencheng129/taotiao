d8a53971eca7ebfba140915b1bcfb259e46486a5
ian <ian@iandeMacBook-Air.local>
2022-05-02 17:35:58 +0800

    finall

--- src/views/Home/MyHome.vue
+++ src/views/Home/MyHome.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="home-container">
     <van-nav-bar title="标题" fixed />
-
-    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-      <ArticleInfo v-for="item in artList" :key="item.id" :title="item.title" :author="item.aut_name" :cmtCount="item.comm_count" :time="item.pubdate" :cover="item.cover"></ArticleInfo>
-    </van-list>
+    <van-pull-refresh v-model="isLoading" :disabled="finished" success-text="刷新成功" @refresh="onRefresh">
+      <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+        <ArticleInfo
+        v-for="item in artList"
+        :key="item.id" :title="item.title"
+        :author="item.aut_name"
+        :cmtCount="item.comm_count"
+        :time="item.pubdate"
+        :cover="item.cover">
+        </ArticleInfo>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -22,18 +30,26 @@ export default {
       limit: 10,
       artList: [],
       loading: true,
-      finished: false
+      finished: false,
+      isLoading: false
     }
   },
   created () {
     this.initArticleList()
   },
   methods: {
-    async initArticleList () {
+    async initArticleList (isRefresh) {
       const { data: res } = await getArticleListAPI(this.page, this.limit)
       // this.artList = res
-      this.artList = [...this.artList, ...res]
-      this.loading = false
+      if (isRefresh) {
+        // 如果是true，就讓上面加載新資料
+        this.artList = [...res, ...this.artList]
+        this.isLoading = false
+      } else {
+        // 如果是沒有true，就會變成undefinde，就讓上面加載新資料
+        this.artList = [...this.artList, ...res]
+        this.loading = false
+      }
 
       if (res.length === 0) {
         this.finished = true
@@ -43,6 +59,11 @@ export default {
       console.log('觸發')
       this.page++
       this.initArticleList()
+    },
+    onRefresh () {
+      console.log('觸發下拉刷新')
+      this.page++
+      this.initArticleList(true)
     }
   },
   components: {
@@ -55,7 +76,7 @@ export default {
 .home-container {
   padding: 50px 0;
   ::v-deep .van-nav-bar {
-    background: rgb(23, 216, 174);
+    // background: rgb(23, 216, 174);
     .van-nav-bar__title {
       color: #fff;
     }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: './',
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      less: {
+        // 若 less-loader 版本小于 6.0，请移除 lessOptions 这一级，直接配置选项。
+        lessOptions: {
+          modifyVars: {
+            // 直接覆盖变量
+            // 'nav-bar-background-color': 'red',
+            // 'border-color': '#eee'
+            // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+            hack: 'true; @import "@/theme.less"'
+          }
+        }
+      }
+    }
+  }
 })
